e2ff93e64210ac6ecb13b483acdf340c4160dd12
ThomasBenten <77158728+ThomasBenten@users.noreply.github.com>
2022-01-17 10:11:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def generateSC(length):
     secretCode = []
     for i in range(0, length):
         secretCode.append(str(random.randint(1, 8)))
-    print("DEBUG: secretCode =", secretCode)
+    #print("DEBUG: secretCode =", secretCode)
     return secretCode
 
 def userGuess():
@@ -84,53 +84,62 @@ while True:
                 break
 
             case 2:  #Smart
-                t1 = time.perf_counter()
-                # Generate all possible guesses
-                possibleGuesses = list(range(10**(SClen-1), 10**SClen))
-                for i in range(0, len(possibleGuesses)):
-                    possibleGuesses[i] = str(possibleGuesses[i])
-                possibleGuesses = [x for x in possibleGuesses if "9" not in x and "0" not in x]
+                useTimer = inputNumber("Amount of repetitions: ")
+                totguesses = 0
+                tottime = 0
+                for i in range(0, useTimer):
+                    t1 = time.perf_counter()
+                    # Generate all possible guesses
+                    guessCount = 0
+                    results = (0, 0)
+                    possibleGuesses = list(range(10**(SClen-1), 10**SClen))
+                    for i in range(0, len(possibleGuesses)):
+                        possibleGuesses[i] = str(possibleGuesses[i])
+                    possibleGuesses = [x for x in possibleGuesses if "9" not in x and "0" not in x]
 
-                while results[0] != SClen:  #Check if guessed correctly
-                    # Submit a random guess from possible guesses
-                    print("\nPossible guesses:", len(possibleGuesses))
-                    guess = random.choice(possibleGuesses)
-                    guessCount += 1
-                    print("Guessed:", guess)
-                    results = checkGuess(guess, SC)
+                    while results[0] != SClen:  #Check if guessed correctly
+                        # Submit a random guess from possible guesses
+                        print("\nPossible guesses:", len(possibleGuesses))
+                        guess = random.choice(possibleGuesses)
+                        guessCount += 1
+                        print("Guessed:", guess)
+                        results = checkGuess(guess, SC)
 
-                    possibleGuesses.remove(guess)  #Remove what is guessed from possible guesses
+                        possibleGuesses.remove(guess)  #Remove what is guessed from possible guesses
 
-                    goodGuesses = []
+                        goodGuesses = []
 
-                    for possibleGuess in possibleGuesses:  #Loop through all possible guesses
-                        possibleGuessArray = list(str(possibleGuess))
-                        guessArray = list(str(guess))
-                        correct = 0
-                        contains = 0
+                        for possibleGuess in possibleGuesses:  #Loop through all possible guesses
+                            possibleGuessArray = list(str(possibleGuess))
+                            guessArray = list(str(guess))
+                            correct = 0
+                            contains = 0
 
-                        # Check how many numbers with same index are equal (guess and possible guess)
-                        for i in range(0, SClen):
-                            if possibleGuessArray[i] == guessArray[i]:
-                                correct += 1
-                                guessArray[i] = "0"
-                                possibleGuessArray[i] = "0"
+                            # Check how many numbers with same index are equal (guess and possible guess)
+                            for i in range(0, SClen):
+                                if possibleGuessArray[i] == guessArray[i]:
+                                    correct += 1
+                                    guessArray[i] = "0"
+                                    possibleGuessArray[i] = "0"
 
-                        for i in range(0, SClen):
-                            if possibleGuessArray[i] != "0" and possibleGuessArray[i] in guessArray:
-                                contains += 1
-                                guessArray[guessArray.index(possibleGuessArray[i])] = "0"
-                                possibleGuessArray[i] = "0"
+                            for i in range(0, SClen):
+                                if possibleGuessArray[i] != "0" and possibleGuessArray[i] in guessArray:
+                                    contains += 1
+                                    guessArray[guessArray.index(possibleGuessArray[i])] = "0"
+                                    possibleGuessArray[i] = "0"
 
-                        if correct == results[0] and contains == results[1]:
-                            goodGuesses.append(possibleGuess)
+                            if correct == results[0] and contains == results[1]:
+                                goodGuesses.append(possibleGuess)
 
-                    possibleGuesses = goodGuesses[:]
+                        possibleGuesses = goodGuesses[:]
 
-                t2 = time.perf_counter()
-                print(t2 - t1)
-
-                print("That's correct! It took", guessCount, "guesses.")
+                    t2 = time.perf_counter()
+                    print(t2 - t1)
+                    tottime += t2 - t1
+                    totguesses += guessCount
+                    print("That's correct! It took", guessCount, "guesses.")
+                    SC = generateSC(SClen)
+                print("That took an average of ", totguesses/useTimer, " guesses. A total of ", tottime, " seconds, with an average of ", tottime/useTimer, " seconds per run")
                 break
 
             case 3:  #Bruno
